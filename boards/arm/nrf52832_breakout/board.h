8edf363e801544c9c3d0ce19e886153163d2d933
liangkangnan <lknlfy@163.com>
2018-11-17 16:02:01 +0800

    board: nrf52832_breakout: fix build error
    Signed-off-by: liangkangnan <lknlfy@163.com>

--- boards/arm/nrf52832_breakout/board.h
+++ boards/arm/nrf52832_breakout/board.h
@@ -9,22 +9,4 @@
 
 #include <soc.h>
 
-/* Push button switch 0 */
-#define SW0_GPIO_PIN     13
-#define SW0_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW0_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Push button switch 1 */
-#define SW1_GPIO_PIN     14
-#define SW1_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW1_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Onboard RED LED 0 */
-#define LED0_GPIO_PIN  17
-#define LED0_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard RED LED 1 */
-#define LED1_GPIO_PIN  18
-#define LED1_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
 #endif /* __INC_BOARD_H */
